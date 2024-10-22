a9c0b00a5a90eb529035516352b981684198341d
Pogoretskiy777 <jpogoretskiy@gmail.com>
2024-10-22 16:36:13 -0400

    Created function stubs and test case

---
+++ PogoretskiyJosephHillCipher.java
@@ -0,0 +1,23 @@
+public class PogoretskiyJosephHillCipher {
+
+  char[] alphabet = new char[26];
+
+  public static int[][] findDecryptionKey(int encryptionKey[][]) {
+    return;
+  }
+
+  public static int[] encrypt(int plaintext[], int encryptionKey[][]) {
+    return;
+  }
+
+  public static int[] decrypt(int ciphertext[], int encryptionKey[][]) {
+    return;
+  }
+
+  public static void main(String args[]) {
+    int[][] encryptionKey = { { 16, 7 }, { 9, 14 } };
+    char[] plaintext = { 'J', 'M', 'U', 'I', 'S', 'C', 'O', 'O', 'L' };
+    char[] ciphertext = { 'M', 'Q', 'G', 'V', 'G', 'Q', 'S', 'M', 'J', 'I' };
+  }
+
+}
